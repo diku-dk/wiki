23acc4b2ee5cc9a727362493a36904a1acc79e9f
fwc817 fwc817 <31923882+lennonhill@users.noreply.github.com>
2020-07-07 10:11:07 +0000

    Fix shebang and update tag. Only TaskProlog has access to the relevant CUDA_* environment variables.

--- conf/taskprolog.sh
+++ conf/taskprolog.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-logger -t slurmctld_log -f <(
+logger -t taskprolog -S 4096 -f <(
 scontrol show job "$SLURM_JOB_ID" |paste -sd' ' |sed 's/  */ /g'
 env |grep -e ^CUDA -e ^SLURM |paste -sd' '
 )
