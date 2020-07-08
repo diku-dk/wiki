50265f1f26bd769207507112c9d5d861bb88fa17
fwc817 fwc817 <31923882+lennonhill@users.noreply.github.com>
2020-07-08 13:25:26 +0000

    Exclude usernames and job names from log

--- conf/prolog.sh
+++ conf/prolog.sh
@@ -2,6 +2,6 @@
 echo "export CUDA_DEVICE_ORDER=PCI_BUS_ID"
 
 (
-scontrol -o show job "$SLURM_JOB_ID"
-env |grep -e ^CUDA -e ^SLURM |paste -sd' '
+scontrol -o show job "$SLURM_JOB_ID" |sed -e 's/\(UserId\|Account\|WorkDir\|JobName\|Command\)=[^ ]*//g'
+env |grep -e ^CUDA -e ^SLURM |grep -ve SLURM_JOB_USER= -e SLURM_SUBMIT_DIR= -e SLURM_JOB_ACCOUNT= -e SLURM_JOB_NAME= |paste -sd' '
 ) 2>/dev/null |logger -t srunprolog -S 3072 >/dev/null 2>&1
